49badbcc89db5e7a7a18e069ed8f3884efc37176
mgarza78 <mikecgarza1978@hotmail.com>
2018-12-18 14:57:43 -0600

    Update FIXDOWN_MANUAL.ps1

--- FIXDOWN_MANUAL.ps1
+++ FIXDOWN_MANUAL.ps1
@@ -4,10 +4,9 @@
     $fold = Get-ChildItem C:\backup\lastbak | ? { $_.PSIsContainer } | sort CreationTime -Descending | select -f 1
     $fold2 = $fold.FullName + $fil
    
-  # Copy-Item 'C:\backup\applicationhost.config' $filename -Force
+  
     Copy-Item $fold2 $filename -Force 
 
 
     sleep 2
-   # Send-MailMessage -To mismailbox@ena.com -From MISBOT@ena.com -Subject "Proxy Fixed" -Body "Verify IIS " -BodyAsHtml   -SmtpServer smtp.corp.ena.net
-
+   

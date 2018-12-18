923ebd929fef851bb23da05a694b55160ed919f3
mgarza78 <mikecgarza1978@hotmail.com>
2018-12-18 15:06:41 -0600

    Add files via upload

---
+++ restV2.ps1
@@ -0,0 +1,39 @@
+﻿
+$i = 0
+
+$username1 = "USERNAME"
+$password1 = "PASSWORD"
+$secstr1 = New-Object -TypeName System.Security.SecureString
+$password1.ToCharArray() | ForEach-Object {$secstr1.AppendChar($_)}
+$cred1 = new-object -typename System.Management.Automation.PSCredential -argumentlist $username1, $secstr1
+New-PSDrive -Name Y -Root "TO YOUR DAG SERVER PATH" -PSProvider FileSystem -Credential $cred1
+
+
+while ($i -le 4)
+
+
+{
+
+Try
+{
+
+Invoke-RestMethod https://YOUDAGSERVER/dag
+
+
+}
+Catch {
+
+
+
+Out-File Y:\down.txt
+
+
+
+}
+
+
+$i++
+
+sleep 65
+
+}

c5d0292767989a9a12086246a108526f88a3db95
mgarza78 <mikecgarza1978@hotmail.com>
2018-12-18 14:57:13 -0600

    Add files via upload
    Fixing IIS if the webconfig is in use while rules are generated. Looks for a path with copied configs and finds one with a value

---
+++ FIXDOWN_MANUAL.ps1
@@ -0,0 +1,13 @@
+﻿$fil = "\applicationHost.config"
+
+    $filename ="C:\Windows\System32\inetsrv\config\applicationHost.config"
+    $fold = Get-ChildItem C:\backup\lastbak | ? { $_.PSIsContainer } | sort CreationTime -Descending | select -f 1
+    $fold2 = $fold.FullName + $fil
+   
+  # Copy-Item 'C:\backup\applicationhost.config' $filename -Force
+    Copy-Item $fold2 $filename -Force 
+
+
+    sleep 2
+   # Send-MailMessage -To mismailbox@ena.com -From MISBOT@ena.com -Subject "Proxy Fixed" -Body "Verify IIS " -BodyAsHtml   -SmtpServer smtp.corp.ena.net
+
